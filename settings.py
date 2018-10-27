2ecf57080449f424e6d813ca6a4f156d0a143705
CyberTailor <CyberTailor@gmail.com>
2018-10-27 14:47:56 +0500

    configurable categories and path to files

--- settings.py
+++ settings.py
@@ -1,1 +1,9 @@
 # Token for vk
+token = None
+
+# Location of categoryname.txt files containing questions
+path = "mysite/bot/question"
+
+categories = ["economic", "social", "national",
+              "traditional", "revolution", "ecological"]
+
