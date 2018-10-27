de758f926fb35fcd5ee9c5d02723b37e6d5b42fc
CyberTailor <CyberTailor@gmail.com>
2018-10-27 14:57:05 +0500

    initialization fix

--- models.py
+++ models.py
@@ -4,18 +4,16 @@ from botFlask import db
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    position = db.Column(db.SmallInteger)
-    category_index = db.Column(db.SmallInteger)
+    position = db.Column(db.SmallInteger, default=0)
+    category_index = db.Column(db.SmallInteger, default=0)
 
     categories = db.relationship("Category", back_populates="user")
 
     def __init__(self, user_id, **kwargs):
         super().__init__(id=user_id, **kwargs)
-        self.position = 0
-        self.category_index = 0
 
     def __repr__(self):
-        return "<User(id={}, categores={}) cat. #{}, quest. #{}>".format(
+        return "<User(id={}, categores={}, C{}Q{}>".format(
             self.id, self.categories, self.category_index, self.position)
 
 
@@ -24,16 +22,12 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     index = db.Column(db.SmallInteger)
     name = db.Column(db.String, nullable=False)
-    points = db.Column(db.Float)
+    points = db.Column(db.Float, default=0)
 
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     user = db.relationship("User", back_populates="categories", uselist=False)
 
     history = db.relationship("HistoryItem", back_populates="category")
-
-    def __init__(self, index, name, user, **kwargs):
-        super().__init__(index=index, name=name, user=user, **kwargs)
-        self.points = 0
 
     def __repr__(self):
         return "<Category(user_id={}, index={}, name={}, points={})>".format(
