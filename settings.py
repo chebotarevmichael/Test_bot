e5472eb47d7ddbaaa1dc1bbf60d2dc95ab6588b3
CyberTailor <CyberTailor@gmail.com>
2018-10-28 11:44:48 +0500

    configurable answer texts and points

--- settings.py
+++ settings.py
@@ -10,3 +10,13 @@ path = "mysite/bot/question"
 categories = ["economic", "social", "national",
               "traditional", "revolution", "ecological"]
 
+# Points user gets choosing answer on bot keyboard
+body_to_ans = {
+    "Полностью согласен": 1,
+    "Скорее согласен": 0.5,
+    "Не знаю | Смешанно": 0,
+    "Скорее не согласен": -0.5,
+    "Полностью не согласен": -1,
+}
+
+back_label = "Назад"
